1bc92b518550e57398d7b7302a18d2f6e7e8fa8c
Tushar Sabhani <tsabhani08@gmail.com>
2022-10-04 23:44:44 +0530

    pushing code for ICO dApp

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7A3D98b7f19727E9CA1B95aA1f19Be21C6b52014";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
